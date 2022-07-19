89ef634c73d0f86f71218aba7bd42848fe02428f
Bianca Rabbi <bianca.rabbi1@gmail.com>
2022-07-19 17:33:22 -0300

    added dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="App-footer">
+          <small>coded by Bianca Rabbi for SheCodes React</small>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`searching for ${keyword} definition...`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="search here"
+          onChange={handleKeywordChange}
+        />
+      </form>
+    </div>
+  );
+}
